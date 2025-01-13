526f49f3286d55b1fc31c68c423636b52441d63b
Camila Braga <milabragalima@hotmail.com>
2025-01-12 21:24:48 -0300

    Tela Intro atualizada

--- src/main/java/imd/principal/sebolunarfx/IntroController.java
+++ src/main/java/imd/principal/sebolunarfx/IntroController.java
@@ -3,10 +3,10 @@ package imd.principal.sebolunarfx;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
-import javafx.scene.control.SplitPane;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
 
 public class IntroController {
 
@@ -36,9 +36,33 @@ public class IntroController {
         introText.setText("Funcionou!");
     }
 
+    //Fazer o programa encerrar!
     @FXML
     protected void onIntroExitButton() {
         introText.setText("Teste para sair!");
+    }
+
+    @FXML
+    public void initialize() {
+
+        Image image = new Image("/luaIntro.jpg");
+
+        // create a background image
+        BackgroundSize backgroundSize = new BackgroundSize(1, 1, true, true, false, false);
+        BackgroundImage backgroundimage = new BackgroundImage(image,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundPosition.CENTER,
+                backgroundSize);
+
+        // create Background
+        Background background = new Background(backgroundimage);
+
+        vBox1.setBackground(background);
+
+
+        introButton.setStyle("-fx-border-color: #c3603a; -fx-background-color: #ff9061;");
+        introExitButton.setStyle("-fx-border-color: #c3603a; -fx-background-color: #ff9061;");
     }
 
 
